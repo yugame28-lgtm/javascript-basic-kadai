f70c2ee8e5e5a9820aeaca6c0e1244f3cdbfda6b
yugame28-lgtm <yugame28@gmail.com>
2026-09-12 08:40:00 +0900

    first commit

---
+++ bme.js
@@ -0,0 +1,9 @@
+// 変数を使って体重と身長を定義する
+let weight = 68;
+let height = 1.7;
+
+// 体重(kg) ÷ (身長(m) × 身長(m)) の計算結果を bmi 変数に代入する
+let bmi = weight / (height * height);
+
+// 計算結果をコンソールに出力する
+console.log(bmi);
